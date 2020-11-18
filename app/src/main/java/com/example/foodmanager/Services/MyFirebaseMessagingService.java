9b351af383b5afc8a5a2abd7bbd470cff98505c4
Sofya Ignatevskaya <57006805+sofya-ignatevskaya@users.noreply.github.com>
2020-11-18 18:37:16 +0300

    build config

--- app/src/main/java/com/example/foodmanager/Services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/foodmanager/Services/MyFirebaseMessagingService.java
@@ -21,26 +21,5 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onNewToken(String token) {
         Log.d(TAG, "New token: " + token);
     }
-
-  /*  @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-       sendNotification(remoteMessage.getNotification().getBody());
-    }
-
-    private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        Uri defSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(this.getString(R.string.app_name))
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setSound(defSoundUri);
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notificationBuilder.build());}*/
-
-
+    
 }
